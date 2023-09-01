17db308e02249026d168dc28d24c4c6361adda0a
Rene Rodriguez <134916213+rodriguezr1016@users.noreply.github.com>
2023-09-01 10:33:51 -0700

    I finished the index.js file which handles all of the user input

--- index.js
+++ index.js
@@ -23,6 +23,29 @@ const questions = [
 }
 ]
 
+function writeToFile(fileName, responses) {
+    let svgString = "";
+    svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += "<g>";
+    svgString += `${responses.shape}`;
+    let shapeChoice;
+  if (responses.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${responses.color}"/>`;
+  } else if (responses.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${responses.color}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${responses.color}"/>`;
+  }
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${responses.textColor}">${responses.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
+     fs.writeFile(fileName, svgString, (err) =>
+    err ? console.error(err) : console.log('Success! Checkout new svg file.'))
+}
 function init() {
     inquirer.prompt(questions).then((responses)=>{
         console.log("Creating svg file...");
